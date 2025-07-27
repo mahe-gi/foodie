cc9e54fee5213bdc6e1e5c28b170e700bb5a7549
Mahesh Ch <chmahesh.dev@gmail.com>
2025-07-27 22:12:04 +0530

    card implimentation

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+const SWIGGY_API =
+  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
+const SWIGGY_IMAGE_URL =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export { SWIGGY_API, SWIGGY_IMAGE_URL };
+
+// data.cards[4].card.card.gridElements.infoWithStyle.restaurants;

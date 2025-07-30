c6579474e9494e361f4e8cb1c1f391d323b5c1b6
Mahesh Ch <chmahesh.dev@gmail.com>
2025-07-30 15:37:15 +0530

    developed  searching resturents box

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,23 +4,70 @@ import { useEffect, useState } from "react";
 
 const Body = () => {
   let [restaurants, setRestuarents] = useState([]);
+  let [filterdArr, setfilteredArr] = useState([]);
+
   useEffect(() => {
     getData();
   }, []);
 
   const getData = async () => {
-    const res = await fetch(SWIGGY_API);
-    const json = await res.json();
-    setRestuarents(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    try {
+      const res = await fetch(SWIGGY_API);
+      const json = await res.json();
+      setRestuarents(
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+      setfilteredArr(
+        json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants
+      );
+    } catch {
+      setfilteredArr([]);
+    }
+  };
+
+  const handleSortRest = () => {
+    const list = restaurants.filter((e) => {
+      return e.info.avgRating > 4.3;
+    });
+    setfilteredArr(list);
+  };
+
+  let val = "";
+  const inputHandler = (e) => {
+    val = e.target.value;
+  };
+
+  const handleSearch = () => {
+    let search = restaurants.filter((e) => {
+      return e.info.name.toLowerCase().includes(val.toLowerCase());
+    });
+    setfilteredArr(search);
   };
 
   return (
     <div id="body">
-      {restaurants.map((e) => {
-        return <Card key={e.info.name} data={e.info} />;
-      })}
+      <div id="search">
+        <input
+          type="text"
+          onChange={inputHandler}
+          placeholder="Search Resturents"
+        />
+        <button onClick={handleSearch}>search</button>
+        <button id="fourstar" onClick={handleSortRest}>
+          4.3 rating above
+        </button>
+      </div>
+      {filterdArr.length == 0 ? (
+        <div>no restaurants found ! </div>
+      ) : (
+        <div id="rest">
+          {filterdArr.map((e) => {
+            return <Card key={e.info.name} data={e.info} />;
+          })}
+        </div>
+      )}
     </div>
   );
 };
